e7204c7a80476f996d45b8b5fc3db1f8b2bd1521
Jeng-Sheng Yeh 葉正聖 <jsyeh@mail.mcu.edu.tw>
2023-02-20 11:22:43 +0800

    add unity project

---
+++ Assets/myRotate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(new Vector3(0, 0.11f, 0));
+    }
+}
